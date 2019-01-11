32041766c7be8e686faa4bcd89a893eaf96c2aa4
Robin Sunny <robinsunny@Robins-MacBook-Pro.local>
2019-01-11 10:34:13 -0800

    feat(base): db migrations

--- config/test.js
+++ config/test.js
@@ -1,10 +1,5 @@
 'use strict';
 
 module.exports = {
-  PORT: 3000,
-  DB_HOST: 'localhost',
-  DB_NAME: 'sfmovies_test',
-  DB_PASSWORD: '',
-  DB_PORT: 5432,
-  DB_USER: 'sfmovies_user'
+  PORT: 3000
 };
