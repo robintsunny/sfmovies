2c8588e4f681e60ae600388ccca9cf530143bb4a
Robin Sunny <robinsunny@Robins-MacBook-Pro.local>
2019-01-16 16:38:44 -0800

    fixes

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -8,8 +8,9 @@ describe('movie controller', () => {
 
     it('creates a movie', async () => {
       const payload = { title: 'WALL-E' };
-
+      
       const movie = await Controller.create(payload)
+
       expect(movie.get('title')).to.eql(payload.title);
 
     });
