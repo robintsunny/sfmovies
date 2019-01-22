bbd9189fe0f91125993f9e984e3f95cfaf79762c
Robin Sunny <robinsunny@Robins-MacBook-Pro.local>
2019-01-22 10:29:28 -0800

    schema column change

--- db/migrations/20190117165648_add_name_to_movies.js
+++ db/migrations/20190117165648_add_name_to_movies.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const TABLE = 'movies';
+const CONSTRAINT_NAME = 'title_not_null';
+
 exports.up = async (Knex) => {
   await Knex.schema.table('movies', (table) => {
     table.text('name')
@@ -8,10 +11,12 @@ exports.up = async (Knex) => {
   return Knex.raw('ALTER TABLE movies ALTER COLUMN title DROP NOT NULL');
 };
 
+
 exports.down = async (Knex) => {
   await Knex.schema.table('movies', (table) => {
     table.dropColumn('name')
   });
 
-  return Knex.raw('ALTER TABLE movies ALTER COLUMN title SET NOT NULL');
-};
+  await Knex.raw(`ALTER TABLE ${TABLE} ADD CONSTRAINT ${CONSTRAINT_NAME} CHECK(title IS NOT NULL) NOT VALID`);
+  return Knex.raw(`ALTER TABLE ${TABLE} VALIDATE CONSTRAINT ${CONSTRAINT_NAME}`);
+}
